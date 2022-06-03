20a62b009230769bf33e9edb73c2e9f07f9c2455
Zhither <m900.eduxa@gmail.com>
2022-06-03 19:37:33 -0300

    test cases added and git ignore

--- SortedSquares.test.js
+++ SortedSquares.test.js
@@ -9,7 +9,15 @@ describe("takes a non-empty array of integers and sorted in ascending order", ()
         expect(SortedSquares([1,2,5])).toStrictEqual([1,4,25]);
     });
 
-    test('["prueba","a","ohh"] should result in "-1"', () => {
-        expect(SortedSquares(["prueba","a","ohh"])).toBe(-1);
+    test('test case 1', () => {
+        expect(SortedSquares([1, 2, 3, 5, 6, 8, 9])).toStrictEqual([1, 4, 9, 25, 36, 64, 81]);
+    });
+
+    test('test case 2', () => {
+        expect(SortedSquares([-2, -1])).toStrictEqual([1, 4]);
+    });
+
+    test('test 3', () => {
+        expect(SortedSquares([-10, -5, 0, 5, 10])).toStrictEqual([0, 25, 25, 100, 100]);
     });
 });
